9d01c14d1215e1841716d7dca952d3aed9791ea3
Seye <seyeadekanye@gmail.com>
2018-02-03 21:33:11 -0500

    Added app.py file
    Refactored code, and added some new features

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ Created on Sat Feb  3 10:56:27 2018
 
 import dash
 import pandas as pd
+import numpy as np
 from datetime import datetime as dt
 from dash.dependencies import Input, Output
 import dash_html_components as html
@@ -26,93 +27,99 @@ professions_options = []
 for profession in professions:
    professions_options.append({'label': profession, 'value': profession})
 
-# professions_options = professions_options[0:6]
+# Generate year list
+date_options_list = np.arange(min(license_data['licence_year']), dt.now().year+3)
+date_options = []
+for date in date_options_list:
+   date_options.append({'label': date, 'value': date})
+
+# Generate random number to slice professions_option 
+# to use for default profession
+num = np.random.randint(0,len(professions_options))
+
     
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.Div([
+   html.Div([
 
 
-    html.H1(['Delaware Licensing and Disciplinary Actions'],style={'textAlign':'center'}),
+       html.H1(['Delaware Licensing and Disciplinary Actions'],style={'textAlign':'center'}),
 
-#left div
-    
-    html.Div([
-        html.Div([
-            html.Label('Select A Profession Type'),
-            dcc.Dropdown(
-                id='profession',
-                options= professions_options,
-                value='',
-                placeholder='Nursing...'
-        )],style={'width': '48%', 'display': 'inline-block'}),
-        html.Div([
-            dcc.DatePickerSingle(
-                id='my-date-1',
-                display_format='YYYY',
-                placeholder='YYYY',
-            ),
-            html.Label('Enter a Year'),
-        ]), 
-            
+   #left div
+       
+       html.Div([
+           html.Div([
+               html.Label('Select A Profession Type'),
+               dcc.Dropdown(
+                   id='profession',
+                   options= professions_options,
+                   value=professions_options[num],
+                   placeholder='Nursing...'
+                )
+            ], style={'width': '48%', 'display': 'inline-block'}),
+            html.Div([
+                html.Label('Select A Year'),
+                dcc.Dropdown(
+                   id='date-1',
+                   options= date_options,
+                   value= date_options[-4],
+               ),
+            ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}), 
+
             html.Div(id='my-div-1', style={'width': '48%', 'display': 'inline-block'}),
 
-    ], style={'width': '48%', 'display': 'inline-block'}
-    ),
+       ], style={'width': '48%', 'display': 'inline-block'}
+       ),
 
-#right div
+   #right div
 
-    html.Div([
-        html.Div([
-            html.Label('Select A Profession Type'),
-            dcc.Dropdown(
-                id='profession-2',
-                options= professions_options,
-                value='',
-                placeholder='Massage Bodywork...'
-        )],style={'width': '48%', 'display': 'inline-block'}),
-        html.Div([
-            dcc.DatePickerSingle(
-                id='my-date-2',
-                display_format='YYYY',
-                placeholder='YYYY',
-            ),
-            html.Label('Enter a Year'),
-        ]), 
-            
+       html.Div([
+           html.Div([
+               html.Label('Select A Profession Type'),
+               dcc.Dropdown(
+                   id='profession-2',
+                   options= professions_options,
+                   value=professions_options[num],
+                   placeholder='Nursing...'
+                )
+            ], style={'width': '48%', 'display': 'inline-block'}),
+            html.Div([
+                html.Label('Select A Year'),
+                dcc.Dropdown(
+                   id='date-2',
+                   options= date_options,
+                   value= date_options[-4],
+               ),
+            ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}), 
+
             html.Div(id='my-div-2', style={'width': '48%', 'display': 'inline-block'}),
 
-    ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
-    ),
+       ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
+       ),
 
-
-    # html.Label('Multi-Select Dropdown'),
-    # dcc.Dropdown(
-    #     options=[
-    #         {'label': 'New York City', 'value': 'NYC'},
-    #         {'label': u'Montréal', 'value': 'MTL'},
-    #         {'label': 'San Francisco', 'value': 'SF'}
-    #     ],
-    #     value=['MTL', 'SF'],
-    #     multi=True
-    # ),
-    ])
+   ])
 ])
 
 @app.callback(
-    Output(component_id='my-div-1', component_property='children'),
-    [Input(component_id='profession', component_property='value')]
+   Output(component_id='my-div-1', component_property='children'),
+   [Input(component_id='profession', component_property='value'),
+    Input(component_id='date-1', component_property='value')]
 )
-def update_output_div_1(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+def update_output_div_1(profession, yr):
+   return 'year: "{}"'.format(str(yr['value']))
 
 @app.callback(
-    Output(component_id='my-div-2', component_property='children'),
-    [Input(component_id='profession-2', component_property='value')]
+   Output(component_id='my-div-2', component_property='children'),
+   [Input(component_id='profession-2', component_property='value'),
+    Input(component_id='date-2', component_property='value')]
 )
-def update_output_div_2(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+def update_output_div_2(profession,yr):
+   return 'You\'ve entered "{}"'.format(profession['value'])
+
+
+
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+   app.run_server(debug=True)
